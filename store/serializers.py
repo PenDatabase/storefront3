69dc754074c5249d5ed69af652c524642c291177
PenDatabase <davideneasatochibueze@gmail.com>
2025-07-16 14:37:25 +0100

    feat: Implemented Product Images in Products being returned

--- store/serializers.py
+++ store/serializers.py
@@ -18,13 +18,18 @@ class ProductImageSerializer(serializers.ModelSerializer):
         model = ProductImage
         fields = ['id', 'image']
 
+    def create(self, validated_data):
+        product_id = self.context['product_id']
+        return ProductImage.objects.create(product_id=product_id, **validated_data)
+
 
 class ProductSerializer(serializers.ModelSerializer):
-    images = ProductImageSerializer(many=True)
+    images = ProductImageSerializer(many=True, read_only=True)
     class Meta:
         model = Product
-        fields = ['id', 'title', 'images', 'description', 'slug', 'inventory',
-                  'unit_price', 'price_with_tax', 'collection']
+        fields = ['id', 'title', 'description', 'slug', 'inventory',
+                  'unit_price', 'price_with_tax', 'collection', 'images']
+        
 
     price_with_tax = serializers.SerializerMethodField(
         method_name='calculate_tax')
