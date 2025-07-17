eaa0e57f81d61478b9844a51fd0b770df431d772
PenDatabase <davideneasatochibueze@gmail.com>
2025-07-17 16:41:25 +0100

    feat: Addedd corsheaders

--- storefront/settings.py
+++ storefront/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_filters',
+    'corsheaders',
     'rest_framework',
     'djoser',
     'playground',
@@ -51,6 +52,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -67,7 +69,14 @@ INTERNAL_IPS = [
     # ...
 ]
 
+
+
 ROOT_URLCONF = 'storefront.urls'
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:8001',
+    'htpp://127.0.0.1:8001'
+]
 
 TEMPLATES = [
     {
