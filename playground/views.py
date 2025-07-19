b85c7dc7b8822f5ffa7a62dad6bf44454f1358d1
PenDatabase <davideneasatochibueze@gmail.com>
2025-07-19 13:08:24 +0100

    feat: Used templated mail to send email

--- playground/views.py
+++ playground/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
-from django.core.mail import mail_admins, BadHeaderError
+from django.core.mail import BadHeaderError
+from templated_mail.mail import BaseEmailMessage
 
 def say_hello(request):
-    # try:
-    #     mail_admins('Infectious', 'hello', html_message='<h1>Hello</h1>')
-    # except BadHeaderError:
-    #     pass
+    try:
+        message = BaseEmailMessage(template_name="emails/hello.html",
+                                   context= {"name": "David"})
+        message.send(["davideneasatochibueze@gmail.com"])
+    except BadHeaderError:
+        pass
     return render(request, 'hello.html', {'name': 'Mosh'})
